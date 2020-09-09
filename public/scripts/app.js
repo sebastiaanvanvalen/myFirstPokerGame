33d490ce2d70add5c1d068b7c68ee7590f00fc8d
sebastiaanvanvalen <sebastiaanvanvalen@gmail.com>
2020-09-09 14:59:13 +0200

    add table setup functions
    added all functions needed to set up the table with the right amount of chairs/players and cards

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,62 @@
-$(document).ready(function(){
-    $(".new_game_btn").on('click', function(){
-       $.ajax({
-           type: 'GET',
-           url: '/application/main/',
-           headers: {
-            //   csrf_token: xxx,
-            reqType: "newGame",
-            client_name: "Sebastiaan",
-            n_o_p: 8 // number of players
-            
-           },
-           async: true,
-       }).done(function (data) {
-           console.log(data);
-       }).fail(function (data) {
-           alert("AJAX request failed");
-           console.log(data);
-       });
+$(document).ready(function () {
+    let csrf_token;
+    let clientName;
+    let NOP = $(".NOP").val();
+
+    $(".submit_NOP_btn").on('click', function () {
+        $.ajax({
+            type: 'GET',
+            url: '/application/main',
+            headers: {
+                //   csrf_token: xxx,
+                reqType: "newGame",
+                client_name: "Sebastiaan",
+                NOP: NOP
+
+            },
+            async: true,
+        }).done(function (data) {
+            // data = json with all players id's, chairNumbers, walletContents and the client's cards
+            let players = JSON.parse(data);
+            console.log(data);
+            setPlayers(players);
+        }).fail(function (data) {
+            alert("AJAX request failed");
+            console.log(data);
+        });
+    })
 })
-})
+
+// have x amount of player occupy the table nice and evenly
+function setPlayers(players) {
+    let NOP = players[0].length;
+    let chairs = [];
+
+    for (let x = 0; x < NOP; x++) {
+        let name   = players[0][x][0];
+        let chair  = players[0][x][1];
+        let wallet = players[0][x][3];
+        let blind;
+        let card_1;
+        let card_2;
+        players[0][x][4] ? card_1 = '<img src="./images/playing_cards/'+ players[0][x][4] +'.png" class="card user_card_1">' : card_1 = '<img src="./images/playing_cards/blue_back.png" class="card user_card_1">';
+        players[0][x][5] ? card_2 = '<img src="./images/playing_cards/'+ players[0][x][5] +'.png" class="card user_card_1">' : card_2 = '<img src="./images/playing_cards/blue_back.png" class="card user_card_2">';
+        
+        if (typeof (name) === 'number') {
+            name = "player_" + players[0][x][0];
+        }
+        
+        if (players[0][x][2] === "BB"){
+            blind = "BB";
+        } else if (players[0][x][2] === "SB") {
+            blind = "sb"
+        } else {
+            blind = ""
+        }
+
+        let playerTemplate = "<div class='chair chair_" + chair + "'><div class='name " + name + "'>" + name + "</div><div class='player_cards'>" + card_1 + card_2 + "</div><div class='blind user_blind'>"+ blind +"</div><div class='user_wallet'><div class='wallet'>" + wallet + "</div><div class='bet'></div></div></div>";
+        chairs.push(chair);
+        $(".table_container").after(playerTemplate);
+    }
+
+}
