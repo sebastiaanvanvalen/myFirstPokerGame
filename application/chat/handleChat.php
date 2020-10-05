d87e83c48ac9052c1a15690c255edc7f0eb224d5
sebastiaanvanvalen <sebastiaanvanvalen@gmail.com>
2020-10-05 14:55:45 +0200

    finished basic layout

---
+++ application/chat/handleChat.php
@@ -0,0 +1,28 @@
+<?php
+// page extends from main.php
+
+// $stmt   = $conn->prepare((\file_get_contents(dirname(__FILE__) . '/sql/saveChat.sql')));
+// $stmt->bindParam(":player_id", $playerId);
+// $stmt->bindParam(":message", $chatMessage);
+// $stmt->execute();
+
+
+
+$ch = curl_init('http://localhost:8080');
+
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+
+$jsonData = json_encode([
+    'dataType' => 'chatMessage',
+    'playerId'       => $playerId,
+    'chatMessage'  => $chatMessage,
+    'playerName' => $playerName
+]);
+
+$query = http_build_query(['data' => $jsonData]);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+curl_close($ch);
