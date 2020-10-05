c82a19d365966669027cf24f5c378839b02900b4
sebastiaanvanvalen <sebastiaanvanvalen@gmail.com>
2020-10-05 14:52:21 +0200

    adjust data flow to client
    from now the data to client actualy represents the playerObjects

--- application/gameSetup/setup.php
+++ application/gameSetup/setup.php
@@ -1,74 +1,33 @@
 <?php
+// page extends from main.php
 
-// create Players and PlayerStyles
-// create Deck with 52 Cards
-// determine blinds position
-// 
-// require_once dirname(dirname(dirname(__FILE__))) . '/vendor/autoload.php';
-
-use application\common\Deck;
 use application\common\Player;
 use application\common\Wallet;
 
-$Deck = new Deck();
-// at this point the Deck is shuffled
-
-// these variables will be passed as player input via main.php
-$NOP         = 10;
-$moneyAmount = 1800;
-$players     = array();
+// match playerId with Players
 $output      = array();
 
-$noc    = [];
-$noc_2  = [0, 5];
-$noc_3  = [0, 3, 6];
-$noc_4  = [0, 2, 5, 7];
-$noc_5  = [0, 2, 4, 6, 8];
-$noc_6  = [0, 1, 3, 5, 7, 9];
-$noc_7  = [0, 1, 2, 4, 5, 7, 8];
-$noc_8  = [0, 1, 2, 4, 5, 6, 7, 9];
-$noc_9  = [0, 1, 2, 3, 4, 6, 7, 8, 9];
-$noc_10 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+$players = unserialize($_SESSION['PLAYERS']);
 
-$noc = ${"noc_" . $NOP};
+      // echo "\n-----(start vardump)\n";
+      // var_dump($players);
+      // echo "\n-----(end vardump)\n";
+foreach($players as $player){
 
-// set Blinds
-$BB = rand(0, $NOP - 1);
-if($BB === 0){
-   $SB = $NOP - 1;
-} else {
-   $SB = $BB - 1;
-}
+   if($playerId === $player['player_id']){
+      $client = unserialize($_SESSION['player_'.$playerId]);
+      $details = array($client->playerId, $client->playerName, $client->wallet, $client->blindType, $client->card_1, $client->card_2);
 
-for($x=0; $x<$NOP; $x++){
-   $chair = $noc[$x];
-   $player = new Player($x, $chair);
-   $player->setCards($Deck->getHand());
-   $player->createWallet(new Wallet($player->id, $moneyAmount));
-
-   if($x === $BB){
-      $player->setBlind("BB");
-   }elseif($x === $SB){
-      $player->setBlind("SB");
+      array_push($output, $details);
    } else {
-      $player->setBlind("NB");
-   };
+      $client = unserialize($_SESSION['player_'.$player['player_id']]);
+      $details = array($client->playerId, $client->playerName, $client->wallet, $client->blindType);
 
-
-
-   $players[] = $player;
-
-   if($player->status === 1){
-      if($player->id === 0){
-         $output[] = array($player->id, $player->chair, $player->blindType, $player->walletContent, $player->card_1, $player->card_2);
-      } else {
-         $output[] = array($player->id, $player->chair, $player->blindType, $player->walletContent);
-      }
+      array_push($output, $details);
    }
 }
-      // echo "\n-----(start vardump)\n";
-      // var_dump($output);
-      // echo "\n-----(end vardump)\n";
 
-echo json_encode(array($output));
+array_push($output);
 
+
+echo json_encode($output);
