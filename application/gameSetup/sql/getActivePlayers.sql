fd621724b796c942f2d502629be32bcfc37fde6a
sebastiaanvanvalen <sebastiaanvanvalen@gmail.com>
2020-10-26 14:31:35 +0100

    added new player variable

--- application/gameSetup/sql/getActivePlayers.sql
+++ application/gameSetup/sql/getActivePlayers.sql
@@ -1,3 +1,3 @@
-SELECT act_table_1.player_id, players.player_name FROM act_table_1 
+SELECT act_table_1.player_id, players.player_name, player_age FROM act_table_1 
 INNER JOIN players ON act_table_1.player_id = players.id
 WHERE act_table_1.active = 1
